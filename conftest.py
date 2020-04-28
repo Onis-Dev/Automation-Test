1a959159f81830fa20e01f317b506897c4ad03aa
Julio Cesar Zendejo <julio.zendejo@onissolutions.com>
2020-04-28 12:25:22 -0500

    saving local changes

--- conftest.py
+++ conftest.py
@@ -9,12 +9,13 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("window-size=1400,600")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = "/usr/bin/google-chrome-stable" 
+# chrome_options.binary_location = "/usr/bin/google-chrome-stable" 
 
 @pytest.fixture(scope="class")
 def setup(request):
     print("initiating chrome driver")
-    web_driver = webdriver.Chrome(options=chrome_options, executable_path=r'/var/lib/jenkins/workspace/Automation/chromedriver')
+    web_driver = webdriver.Chrome(options=chrome_options)
+    # web_driver = webdriver.Chrome(options=chrome_options, executable_path=r'/var/lib/jenkins/workspace/Automation/chromedriver')
     web_driver.get("https://smoothapps.com/")
     request.cls.driver = web_driver
 
